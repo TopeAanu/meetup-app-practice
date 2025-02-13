ec507e26156da1aa0d47e4038e87b2290870b9ba
TopeAanu <topefasasi99@gmail.com>
2025-02-13 01:42:52 +0100

    added new components

---
+++ app/[meetupId]/page.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/MeetupDetail";
+
+function MeetDetails() {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
+      title="First meetup"
+      address="Some street 5, some city"
+      description="This is a first meetup"
+    />
+  );
+}
+
+export default MeetDetails;
