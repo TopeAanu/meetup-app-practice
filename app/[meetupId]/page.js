2b437bc10172f83e79e864656c90b79c12ed8e21
TopeAanu <topefasasi99@gmail.com>
2025-02-14 23:58:28 +0100

    added some updates

--- app/[meetupId]/page.js
+++ app/[meetupId]/page.js
@@ -1,14 +1,34 @@
-import MeetupDetail from "../../components/MeetupDetail";
+import MeetupDetail from '../../components/meetups/MeetupDetail';
 
-function MeetDetails() {
+// Example data for demonstration
+const DUMMY_MEETUPS = {
+  m1: {
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+    title: 'First Meetup',
+    address: 'Some Street 5, Some City',
+    description: 'This is a first meetup',
+  },
+  m2: {
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+    title: 'Second Meetup',
+    address: 'Another Street 10, Some City',
+    description: 'This is a second meetup',
+  },
+};
+
+// MeetupDetails component as a server component
+export default async function MeetupDetails({ params }) {
+  const meetupId = params.meetupId;
+
+  // Fetch data for a single meetup
+  const meetupData = DUMMY_MEETUPS[meetupId] || {};
+
   return (
     <MeetupDetail
-      image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
-      title="First meetup"
-      address="Some street 5, some city"
-      description="This is a first meetup"
+      image={meetupData.image}
+      title={meetupData.title}
+      address={meetupData.address}
+      description={meetupData.description}
     />
   );
 }
-
-export default MeetDetails;
